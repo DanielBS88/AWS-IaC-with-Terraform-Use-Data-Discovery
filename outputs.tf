667d94e7ff2c268288953dd0973d4490cd427451
admin <daniel_brito@epam.com>
2026-01-14 22:43:51 -0300

    Fix output name to match lab validator expectations

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,5 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
+output "cmtr-k5vl9gpq-instance" {
+  description = "EC2 instance ID for lab validation"
   value       = aws_instance.cmtr_k5vl9gpq_instance.id
 }
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.cmtr_k5vl9gpq_instance.public_ip
-}
