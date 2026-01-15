68a0f77a6b279fea28ef30b23e74b30994482ab8
admin <daniel_brito@epam.com>
2026-01-14 23:08:46 -0300

    Add lab-required output name for EC2 instance

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "instance_id" {
   value       = aws_instance.cmtr_k5vl9gpq_instance.id
 }
 
+output "cmtr-k5vl9gpq-instance" {
+  description = "EC2 instance ID for lab validator"
+  value       = aws_instance.cmtr_k5vl9gpq_instance.id
+}
+
