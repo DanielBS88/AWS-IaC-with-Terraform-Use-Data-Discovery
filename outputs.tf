a9db67f7531059740c9dfb82374456666a90cae2
admin <daniel_brito@epam.com>
2026-01-14 22:57:57 -0300

    Fix EC2 SG attachment using VPC-compatible configuration

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
-output "cmtr-k5vl9gpq-instance" {
-  description = "EC2 instance ID for lab validation"
+output "instance_id" {
+  description = "EC2 instance ID"
   value       = aws_instance.cmtr_k5vl9gpq_instance.id
 }
 
