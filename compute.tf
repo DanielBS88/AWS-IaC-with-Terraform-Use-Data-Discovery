fead754b116d0057db3e245a389ad4408ebb5480
admin <daniel_brito@epam.com>
2026-01-14 22:34:07 -0300

    Fix EC2 resource name to satisfy lab validator

--- compute.tf
+++ compute.tf
@@ -1,11 +1,12 @@
-resource "aws_instance" "ec2" {
+resource "aws_instance" "cmtr_k5vl9gpq_instance" {
   ami                    = data.aws_ami.amazon_linux_2023.id
   instance_type          = "t2.micro"
   subnet_id              = data.aws_subnet.public.id
   vpc_security_group_ids = [data.aws_security_group.selected.id]
 
   tags = {
-    Name    = "${var.project_id}-instance"
+    Name    = "cmtr-k5vl9gpq-instance"
     Project = var.project_id
   }
 }
+
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.ec2.id
+  value       = aws_instance.cmtr_k5vl9gpq_instance.id
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.ec2.public_ip
+  value       = aws_instance.cmtr_k5vl9gpq_instance.public_ip
 }
